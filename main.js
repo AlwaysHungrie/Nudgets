26df4194e1c42dc8441babff2cd2b0bdbec99231
AlwaysHungrie <dhairyashah98@gmail.com>
2017-09-21 17:05:47 +0530

    login 3 type

--- main.js
+++ main.js
@@ -1,13 +1,84 @@
 var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValid = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+
+//connecting to db
+mongoose.connect('mongodb://dhairyaShah:gocashless@ds131384.mlab.com:31384/database-gods')
+var db = mongoose.connection;
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+//Initalise the app
 var app = express();
 
-app.use(express.static(__dirname + 'public'));
+//init view engine(handlebars)
+app.set('views',path.join(__dirname,'views'));
+app.engine('handlebars',exphbs(/*{defaultLayout:'layout'}*/));
+app.set('view engine','handlebars');
 
-app.get('/',function(req,res){
-	res.send('hello world');
+//bodyParse ???
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+//set the static folder
+app.use(express.static(__dirname + '/public'));
+
+//express session
+app.use(session({
+	secret: 'secret',
+	saveUninitialized:true,
+	resave: true
+}));
+
+//passport init(for authentication)
+app.use(passport.initialize());
+app.use(passport.session());
+
+//express validator
+app.use(expressValid({
+	errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
 });
+
+app.use('/',routes);
+app.use('/users',users);
 
 app.set('port',(process.env.port||3030));
 app.listen(app.get('port'),function(){
 	console.log('Server listening on port: '+app.get('port'));
-})
+});
